47e4655495d9d3da16334d036ee4987a1de8c1f9
TrinhDang <dangtrinh768@gmail.com>
2026-07-14 09:59:29 +0700

    Update: main page with footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header.jsx";
 import Home from "./layouts/Home.jsx";
+import Footer from "./components/Footer.jsx";
 function App() {
     return(
         <main>
             <Header/>
             <Home/>
+            <Footer/>
         </main>
     )
 }export default App;
