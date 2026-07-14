1ab58ff10d5828ad6a2fd9019692834fd1e60a51
TrinhDang <dangtrinh768@gmail.com>
2026-07-14 17:04:24 +0700

    Update: remove footer and add it in home

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Header from "./components/Header.jsx";
 import Home from "./layouts/Home.jsx";
-import Footer from "./components/Footer.jsx";
 function App() {
     return(
         <main>
             <Header/>
             <Home/>
-            <Footer/>
         </main>
     )
 }export default App;
